c34acc16de3b7ee20b9f15d7fed91086e43750fa
booserg <syevtushik@gmail.com>
2018-12-21 13:08:35 +0100

    initial

---
+++ ModbusAsciiMaster/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Modbus.Device;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModbusAsciiMaster
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IModbusSerialMaster master;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var masterPort = new SerialPort("COM4");
+            masterPort.BaudRate = 9600;
+            masterPort.DataBits = 8;
+            masterPort.Parity = Parity.None;
+            masterPort.StopBits = StopBits.One;
+            masterPort.ReadTimeout = 100;
+            masterPort.Open();
+
+            master = ModbusSerialMaster.CreateAscii(masterPort);
+            master.Transport.ReadTimeout = 1000;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            master.Dispose();
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var res = await GetData().ConfigureAwait(true);
+
+            tbData.Text = res[0].ToString();
+        }
+
+        private async Task<ushort[]> GetData()
+        {
+            var res = await Task<ushort[]>.Factory.StartNew(() => {
+                byte slaveId = 2;
+
+                ushort[] registers = master.ReadHoldingRegisters(slaveId, 2, 1);
+
+                return registers;
+            });
+            return res;
+        }
+    }
+}
---
+++ ModbusAsciiSlave/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Net;
+
+using System.Net.Sockets;
+
+using System.Threading;
+
+using Modbus.Data;
+
+using Modbus.Device;
+using Modbus.IO;
+
+using Modbus.Utility;
+
+namespace ModbusAsciiSlave
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ModbusSlave slave;
+        SerialPort slavePort;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var t = Task.Factory.StartNew(() =>
+            {
+                slavePort = new SerialPort("COM3");
+
+                slavePort.BaudRate = 9600;
+
+                slavePort.DataBits = 8;
+
+                slavePort.Parity = Parity.None;
+
+                slavePort.StopBits = StopBits.One;
+
+                slavePort.Open();
+
+                byte unitId = 2;
+
+                slave = ModbusSerialSlave.CreateAscii(unitId, slavePort);
+
+                slave.DataStore = DataStoreFactory.CreateDefaultDataStore();
+                slavePort.DiscardInBuffer();
+                slavePort.DiscardOutBuffer();
+                slave.Listen();
+            });
+        }
+        
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            slavePort.Close();
+            slave.Dispose();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (slave != null && slave.DataStore != null)
+            {
+                int n;
+                if (int.TryParse(tbData.Text, out n))
+                {
+                    lock (slave.DataStore.SyncRoot)
+                    {
+                        slave.DataStore.HoldingRegisters[3] = (ushort)n;
+                    }
+                }
+            }
+        }
+    }
+}
